9d1dab45d02c50f2434105720390c2c9fb092bd0
Negi <jerickson576@gmail.com>
2025-06-01 13:19:39 -0500

    feat: Agrega la carga de datos

--- transformacion.py
+++ transformacion.py
@@ -1,8 +1,15 @@
 import pandas as pd
 # esta es la api de opendota API_URI = "https://api.opendota.com/api/matches/8317181521"
 def transformar_datos(raw_json):
-    partida = raw_json
-    partida.pop('players', None) # Eliminar los datos de jugadores porque es demasiado grande para que quede solo partidas ps
+    # Claves a eliminar para poder insertar bien en la tabla
+    claves_considerar = [
+        'match_id', 'start_time', 'duration', 'radiant_win', 'radiant_score',
+        'dire_score', 'radiant_name', 'dire_name', 'radiant_team_id', 'dire_team_id', 'leagueid',
+        'game_mode', 'lobby_type', 'human_players', 'cluster'
+    ]
+    
+    partida = {clave: raw_json.get(clave) for clave in claves_considerar}
+
     df = pd.DataFrame([partida])
     return df
 
